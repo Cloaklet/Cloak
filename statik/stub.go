8360fbd49d002b058864fe2787835b97f49154bf
Joker_ <1465267+JokerQyou@users.noreply.github.com>
2020-09-18 13:50:53 +0800

    Remove actual statik assets module

---
+++ statik/stub.go
@@ -0,0 +1,7 @@
+package statik
+
+/*
+This file contains no code.
+During release building `mage` will call `statik` tool to generate `statik.go` inside this directory
+which will contain all the static assets for UI rendering.
+*/
