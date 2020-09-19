2a693b8b57ca0bc88dc94a2b4c9adb3d24d5d5d8
Joker_ <1465267+JokerQyou@users.noreply.github.com>
2020-09-19 19:05:56 +0800

    Support dynamically changing language

---
+++ frontend/src/components/AppConfigModal.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="modal active" @keydown.esc="close">
+    <a class="modal-overlay" aria-label="Close" @click="close"></a>
+    <div class="modal-container">
+      <div class="modal-header">
+        <a class="btn btn-clear float-right"
+           aria-label="Close"
+           @click="close"></a>
+        <div class="modal-title h5" v-t="'config.title'"></div>
+      </div>
+      <div class="modal-body p-0 bg-gray">
+        <div class="content">
+          <ul class="tab tab-block">
+            <li class="tab-item"
+                :class="{ active: active === 'options' }"
+                @click="active = 'options'">
+              <a v-t="'config.title'"></a>
+            </li>
+            <li class="tab-item"
+                :class="{ active: active === 'about' }"
+                @click="active = 'about'">
+              <a v-t="'config.about.title'"></a>
+            </li>
+          </ul>
+          <div class="p-2" v-if="active === 'options'">
+            <div class="form-horizontal">
+              <div class="form-group">
+                <div class="col-3">
+                  <label class="form-label" for="options-lang" v-t="'config.lang.label'"></label>
+                </div>
+                <div class="col-9">
+                  <select id="options-lang"
+                          class="form-select"
+                          :value="lang"
+                          @change="changeLanguage">
+                    <option>en</option>
+                    <option>zh-Hans</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="p-2" v-if="active === 'about'">
+            // To be done...
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppConfigModal",
+  computed: {
+    lang() {
+      return this.$root.$i18n.locale
+    }
+  },
+  data: function() {
+    return {
+      active: 'options'
+    }
+  },
+  methods: {
+    close() {
+      this.$emit('close')
+    },
+    changeLanguage(event) {
+      this.$root.$i18n.locale = event.target.value
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -4,18 +4,34 @@
       <section class="navbar-section">
         <a class="navbar-brand text-bold text-light text-uppercase">Cloak</a>
       </section>
+      <section class="navbar-section">
+        <a class="btn btn-link text-light" @click="showConfigModal = true">
+          <i class="ri-settings-3-fill ri-xl"></i>
+        </a>
+      </section>
     </header>
+    <AppConfigModal v-if="showConfigModal" @close="showConfigModal = false"/>
   </div>
 </template>
 
 <script>
+import AppConfigModal from "@/components/AppConfigModal";
 export default {
-  name: "AppHeader"
+  name: "AppHeader",
+  components: {AppConfigModal},
+  data: function() {
+    return {
+      showConfigModal: false
+    }
+  }
 }
 </script>
 
 <style scoped>
 header {
   padding: .7rem;
+}
+i {
+  vertical-align: middle;
 }
 </style>
