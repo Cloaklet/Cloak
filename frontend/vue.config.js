5c3d774534063e83b14d99bbbe44b14bac25ba65
Joker_ <1465267+JokerQyou@users.noreply.github.com>
2020-09-20 15:34:17 +0800

    Disable webpack plugins to speed up building

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,19 +1,19 @@
-const FontminPlugin = require('fontmin-webpack')
-const NukeCssPlugin = require('nukecss-webpack')
+// const FontminPlugin = require('fontmin-webpack')
+// const NukeCssPlugin = require('nukecss-webpack')
 
 module.exports = {
   productionSourceMap: false,
 
   chainWebpack: config => {
-    const fontsRule = config.module.rule('fonts')
-    fontsRule.uses.clear()
-    fontsRule
-      .use('file-loader')
-      .loader('file-loader')
-      .end()
+    // const fontsRule = config.module.rule('fonts')
+    // fontsRule.uses.clear()
+    // fontsRule
+    //   .use('file-loader')
+    //   .loader('file-loader')
+    //   .end()
 
-    config.plugin('NukeCssPlugin')
-      .use(NukeCssPlugin)
+    // config.plugin('NukeCssPlugin')
+    //   .use(NukeCssPlugin)
 
     // FontMin plugin greatly impacts the build time (+~300s),
     // disable it for now.
