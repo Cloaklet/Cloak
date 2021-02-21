d8992e9fe9f08d8794fda054d345d5a423083257
u <1465267+JokerQyou@users.noreply.github.com>
2021-02-21 23:41:40 +0800

    Allow GET /api/options request to avoid confusing error messages in `en` locale

--- server/middleware.go
+++ server/middleware.go
@@ -2,12 +2,19 @@ package server
 
 import (
 	"github.com/labstack/echo/v4"
+	"net/http"
 )
 
 // CheckRuntimeDeps is a labstack/echo middleware.
 // It checks for necessary external runtime dependencies, and block API requests if they are not all met.
 func (s *ApiServer) CheckRuntimeDeps(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
+		// Always allow `GET /options` since it's the initial request sent by the UI.
+		// TODO Need a more elegant way to do this
+		if c.Path() == "/api/options" && c.Request().Method == http.MethodGet {
+			return next(c)
+		}
+
 		if !s.fuseAvailable {
 			return ErrMissingFuse
 		}
